741d47be9447dea61e4d175e62ebb212f5ee1515
i393513 <rene.kultz@bradesco.com.br>
2019-06-28 10:15:14 -0300

    adicionada página de formulários de categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDatabase} from './in-memory-database';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+import {Category} from './pages/categories/shared/category.model';
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb() {
+        const categories: Category[] = [
+            {id: 1, name: 'Moradia', description: 'Pagamento de contas da casa'},
+            {id: 2, name: 'Saúde', description: 'Plano de Saúde e medicamentos'},
+            {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
+            {id: 4, name: 'Salário', description: 'Recebimento de Salário'},
+            {id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'},
+        ];
+        return {categories};
+    }
+}
