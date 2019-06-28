f862671c859883a1d08abf9cc1be6da0f62ad034
i393513 <rene.kultz@bradesco.com.br>
2019-06-28 18:23:20 -0300

    Adicionada lista dinâmica de lançamentos

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -7,16 +7,24 @@ export class InMemoryDatabase implements InMemoryDbService {
             {id: 1, name: 'Moradia', description: 'Pagamento de contas da casa'},
             {id: 2, name: 'Saúde', description: 'Plano de Saúde e medicamentos'},
             {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
-            {id: 4, name: 'Salário', description: 'Recebimento de Salário'},
-            {id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'},
+            {id: 4, name: 'Automóvel', description: 'Combustível, '},
+            {id: 5, name: 'Salário', description: 'Recebimento de Salário'},
+            {id: 6, name: 'Freelas', description: 'Trabalhos como freelancer'},
         ];
         const entries: Entry[] = [
             {id: 1, name: 'Gás de Cozinha', categoryId: categories[0].id, 
             category: categories[0], paid: true, date: '10/05/2019', 
-            amount: '70,80', description: 'Gás de cozina', type: 'despesa'},
+            amount: '70,80', description: 'Gás de cozina', type: Entry.types.expense},
             {id: 2, name: 'Salário', categoryId: categories[4].id, 
             category: categories[4], paid: true, date: '05/05/2019', 
-            amount: '1500,00', description: 'Salário mensal', type: 'receita'}];
+            amount: '1500,00', description: 'Salário mensal', type: Entry.types.renevue},
+            {id: 3, name: 'Gasolina', categoryId: categories[3].id, 
+            category: categories[3], paid: true, date: '10/05/2019', 
+            amount: '100,00', type: Entry.types.expense},
+            {id: 4, name: 'Serviço', categoryId: categories[5].id, 
+            category: categories[5], paid: false, date: '10/05/2019', 
+            amount: '2000,00', type: Entry.types.renevue, description:"Pagamento da construção do app"}
+        ];
         return {categories, entries};
     }
 }
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -54,12 +54,15 @@ export class EntryService {
 
   private jsonDataToEntries(jsonData: any[]): Entry[] {
     const entries: Entry[] = [];
-    jsonData.forEach(element => entries.push(element as Entry));
+    jsonData.forEach(element => {
+      const entry = Object.assign(new Entry(), element);
+      entries.push(entry);
+    });
     return entries;
   }
 
   private jsonDataToEntry(jsonData: any): Entry {
-    return jsonData as Entry;
+    return Object.assign(new Entry(), jsonData);
   }
   private handleError(error: any) {
     console.log('Erro na requisição: ', error);
